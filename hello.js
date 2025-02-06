a63cce2e207ccf56a5f8a4b0740db798125e6502
Your Name <you@example.com>
2025-02-06 14:28:49 +0200

    change files to object

--- hello.js
+++ hello.js
@@ -1,2 +1,11 @@
 //console for hello,john
-console.log("hello , john")
+function groupMembers() {
+    const members = {
+        groupleader: 'Abizera Dieu Merci',
+        member1:'Obed Byiringiro',
+        member2:'hirwa bonfis aruno',
+    }
+    console.log(members);
+    console.log("teacher thank you for your task");console.log("teacher thank you for your task");
+}
+alert("our group says thank you for your task");
